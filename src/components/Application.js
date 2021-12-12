cc7545cb69940b6f40ed95da4d25ea46a71a989e
Abdullahi Sabriye <abdullahisabriye@gmail.com>
2021-12-12 15:52:19 -0500

    Add Daylist & Interviewer List

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 
+import DayList from "./DayList";
+
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
@@ -10,6 +32,8 @@ export default function Application(props) {
           className="sidebar--centered"
           src="images/logo.png"
           alt="Interview Scheduler"
+        />
+        <DayList days={days} day={day} setDay={setDay}
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu"></nav>
@@ -21,6 +45,7 @@ export default function Application(props) {
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
+
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
